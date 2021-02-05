20f7b7e5aee7e3b92493e0b74d82fbce30e28ced
victoriatrac <victoriatrac@gmail.com>
2021-02-04 20:53:26 -0700

    finished mvp for part 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: black;
+    color: white;
+    margin-bottom: 5rem;
+  }  
+  img {
+    width: 100%;
+    height: auto;
+  }
+`
+
+const Width = styled.div`
+  width: 80%;
+  margin: auto;
+`
+
+const StyledInfo = styled.div`
+  width: 45%;
+  display: inline-flex;
+  justify-content: center;
+`
 
 function App() {
   const [ photo, setPhoto ] = useState([]);
@@ -16,27 +40,61 @@ function App() {
   useEffect(fetchPhoto, []);
 
   const Photo = (props) => {
+    return (
+      <div className="photo">
+        <h2>{props.title}</h2>
+        <img src={props.url} alt="NASA APOD" />
+      </div>
+    )
+  }
+
+  const Info = (props) => {
+    return (
+      <div className="info">
+        <InfoBar
+          media_type={photo.media_type}
+          hasAName={false}
+          date={photo.date}
+        />
+        <div className="explanation">
+          <p>{props.explanation}</p>
+        </div>
+      </div>
+    )
+  }
+
+  const InfoBar = (props) => {
+    const hasAName = props.hasAName;
+    if (hasAName) {
       return (
-        <div className="photo">
-          <h2>{props.title}</h2>
-          <img src={props.url} alt="NASA APOD" />
-          <div className="infoBar">
+        <div>
+          <StyledInfo>
             <p>{props.media_type} by {props.copyright}</p>
-          </div>
-          <div className="infoBar">
+          </StyledInfo>
+          <StyledInfo>
             <p>{props.date}</p>
-          </div>
-          <div className="explanation">
-            <p>{props.explanation}</p>
-          </div>
+          </StyledInfo>
         </div>
       )
-    // }
+    } return (
+      <div>
+        <StyledInfo>
+          <p>{props.media_type}, photographer unknown</p>
+        </StyledInfo>
+        <StyledInfo>
+          <p>{props.date}</p>
+        </StyledInfo>
+      </div>
+    )
   }
 
   return (
     <div className="App">
-      <div className="shell">
+      <React.Fragment>
+        <GlobalStyle />
+      </React.Fragment>
+    
+      <Width>
         <div className="header">
           <h1>NASA's APOD</h1>
         </div>
@@ -44,12 +102,12 @@ function App() {
           key={photo.date}
           url={photo.url}
           title={photo.title}
-          media_type={photo.media_type}
+        />
+        <Info
           copyright={photo.copyright}
-          date={photo.date}
           explanation={photo.explanation}
         />
-      </div>
+      </Width>
     </div>
   );
 }
